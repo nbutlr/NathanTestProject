ce27272d49b8c51bdd0a17ed928a2e6f70e004ab
nbutlr <05257@farnborough.ac.uk>
2020-11-13 09:53:55 +0000

    changing into a program from repl as the other program was trash

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,15 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        String greeting = "Hello world!";
-        System.out.println(greeting);
-        System.out.println(greeting.length());
-        String greeting2 = "Yo whats up";
-        System.out.println(greeting2);
-        System.out.println(greeting.length()+greeting2.length());
+        int[] scoreList = {98,78,66,81,66,77,12,105,26,87,89,67};
+        int highest = scoreList[0];
+        for (int i = 0; i < 12; i++) {
+            if (scoreList[i] > highest) {
+                highest = scoreList[i];
+            }
+        }
+        System.out.println(highest);
+        System.out.println(Arrays.toString(scoreList));
     }
 }
